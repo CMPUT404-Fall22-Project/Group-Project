3fcc3d8c8302e5f77faded7e0b7d7d107f51437a
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-10-19 01:36:13 -0600

    edit author model and make initial migration

--- backend/authors/models.py
+++ backend/authors/models.py
@@ -5,7 +5,11 @@ class Author(models.Model):
     type = "author"
     id = models.CharField(unique=True, primary_key=True, max_length=255) # e.g. '9de17f29c12e8f97bcbbd34cc908f1baba40658e'
     host = models.URLField(blank=False) # e.g. "http://127.0.0.1:5454/"
-    displayName = models.TextField(null=True) # e.g. "Lara Croft"
-    url = host + "authors/" + id # e.g. "http://127.0.0.1:5454/authors/9de17f29c12e8f97bcbbd34cc908f1baba40658e"
-    github = models.URLField(blank=False) # e.g. "http://github.com/laracroft"
-    profileImage = models.TextField(null=True) # e.g. "https://i.imgur.com/k7XVwpB.jpeg"
+    displayName = models.CharField(max_length=255) # e.g. "Lara Croft"
+    url = str(host) + "authors/" + str(id) # e.g. "http://127.0.0.1:5454/authors/9de17f29c12e8f97bcbbd34cc908f1baba40658e"
+    github = models.URLField() # e.g. "http://github.com/laracroft"
+    profileImage = models.URLField() # e.g. "https://i.imgur.com/k7XVwpB.jpeg"
+
+    def __str__(self):
+        # set displayName as string representation of object
+        return self.displayName
