fff810bc62adbee405dc14633364bc58958c2f8a
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-10-19 14:14:41 -0600

    change DATABASE to postgres

--- backend/project_404/settings.py
+++ backend/project_404/settings.py
@@ -76,10 +76,18 @@ WSGI_APPLICATION = 'project_404.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'project_404',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': 5432,
+
     }
+
 }
 
 
