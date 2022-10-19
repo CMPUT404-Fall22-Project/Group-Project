7a7ae188bc4f01900f5fa68b1fe89c1edce1f5a3
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-10-19 00:07:02 -0600

    create Author Model
    Currently representing id as "9de17f29c12e8f97bcbbd34cc908f1baba40658e" rather than "http://127.0.0.1:5454/authors/9de17f29c12e8f97bcbbd34cc908f1baba40658e"

--- backend/authors/models.py
+++ backend/authors/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    
+    type = "author"
+    id = models.CharField(unique=True, primary_key=True, max_length=255) # e.g. '9de17f29c12e8f97bcbbd34cc908f1baba40658e'
+    host = models.URLField(blank=False) # e.g. "http://127.0.0.1:5454/"
+    displayName = models.TextField(null=True) # e.g. "Lara Croft"
+    url = host + "authors/" + id # e.g. "http://127.0.0.1:5454/authors/9de17f29c12e8f97bcbbd34cc908f1baba40658e"
+    github = models.URLField(blank=False) # e.g. "http://github.com/laracroft"
+    profileImage = models.TextField(null=True) # e.g. "https://i.imgur.com/k7XVwpB.jpeg"
