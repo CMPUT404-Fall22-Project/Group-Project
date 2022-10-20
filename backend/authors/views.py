f02fdb784342f6209a977f7cab05f9530249e831
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-10-20 11:02:49 -0600

    Added some views for FollowerDetail class

--- backend/authors/views.py
+++ backend/authors/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -12,7 +11,7 @@ from .serializers import AuthorSerializer
 # /authors/ GET, POST
 class AuthorList(APIView):
         
-    def get(self, request, fomat=None):
+    def get(self, request, format=None):
         authors = Author.objects.all()
         serializer_arr = []
         for author in authors:
@@ -21,7 +20,8 @@ class AuthorList(APIView):
             serializer_data["id"] = author.get_full_path()
             serializer_data["url"] = author.get_full_path()
             serializer_arr.append(serializer_data)
-        return Response(serializer_arr, status=status.HTTP_200_OK)
+        dict = {"type": "authors", "items": serializer_arr}
+        return Response(dict, status=status.HTTP_200_OK)
 
     
     def post(self, request, format=None):
@@ -34,17 +34,12 @@ class AuthorList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# /authors/<pk> GET, POST
+# /authors/<id> GET, POST
 class AuthorDetail(APIView):
 
-    def get_object(self, pk):
-        try:
-            return Author.objects.get(pk=pk)
-        except Author.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        author = self.get_object(pk)
+    def get(self, request, id, format=None):
+        
+        author = get_object_or_404(Author, id=id)
         serializer = AuthorSerializer(author)
         serializer_data = serializer.data
         serializer_data["id"] = author.get_full_path()
@@ -53,18 +48,14 @@ class AuthorDetail(APIView):
     
 
 class FollowerList(APIView):
-
-    def get_object(self, pk):
-        try:
-            return Author.objects.get(pk=pk)
-        except Author.DoesNotExist:
-            raise Http404
         
-    def get(self, request, pk, fomat=None):
-        author = AuthorDetail.get_object(pk)
+    def get(self, request, id, format=None):
+        """Get all followers of a specified Author"""
+        author = get_object_or_404(Author, id=id)
         followers = author.followers.all()
         serializer = AuthorSerializer(followers, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        dict = {"type": "followers", "items": serializer.data}
+        return Response(dict, status=status.HTTP_200_OK)
 
     
     # def post(self, request, format=None):
@@ -78,19 +69,32 @@ class FollowerList(APIView):
     #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# /authors/<pk> GET, POST
-# class FollowerDetail(APIView):
+# /authors/<id> GET, POST
+class FollowerDetail(APIView):
 
-#     def get_object(self, pk):
-#         try:
-#             return Author.objects.get(pk=pk)
-#         except Author.DoesNotExist:
-#             raise Http404
+    def get(self, request, author_id, follower_id, format=None):
+        """Get a specific Author that is following another specific Author"""
 
-#     def get(self, request, pk, format=None):
-#         author = self.get_object(pk)
-#         serializer = AuthorSerializer(author)
-#         serializer_data = serializer.data
-#         serializer_data["id"] = author.get_full_path()
-#         serializer_data["url"] = author.get_full_path()
-#         return Response(serializer_data, status=status.HTTP_200_OK)
+        author = get_object_or_404(Author, id=author_id)
+        followers = author.followers.all()
+        follower = get_object_or_404(followers, id=follower_id)
+        serializer = AuthorSerializer(follower)
+        serializer_data = serializer.data
+        serializer_data["id"] = author.get_full_path()
+        serializer_data["url"] = author.get_full_path()
+        return Response(serializer_data, status=status.HTTP_200_OK)
+    
+    def put(self, request, author_id, follower_id, format=None):
+        """Add an Author as a follower of another Author"""
+        author = get_object_or_404(Author, id=author_id)
+        follower = get_object_or_404(Author, id=follower_id)
+        author.followers.add(follower)
+        return Response(status=status.HTTP_200_OK)
+    
+    def delete(self, request, author_id, follower_id, format=None):
+        """Remove an Author as a follower of another Author"""
+        author = get_object_or_404(Author, id=author_id)
+        followers = author.followers.all()
+        follower = get_object_or_404(followers, id=follower_id)
+        author.followers.remove(follower)
+        return Response(status=status.HTTP_200_OK)
