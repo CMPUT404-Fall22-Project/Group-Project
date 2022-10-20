e1c9a0dd3edaa309b97d22a6a0c818ab5796a35b
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-10-19 23:01:41 -0600

    add followers to urls.py

--- backend/authors/urls.py
+++ backend/authors/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
 from . import views
 
 # Set up endpoints for CRUD operations
 urlpatterns = [
     path("authors/", views.AuthorList.as_view()),
     path("authors/<pk>/", views.AuthorDetail.as_view()),
+    path("authors/<pk>/followers/", views.FollowerList.as_view()),
 ]
