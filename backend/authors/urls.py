3be9fcd7808338aea09014dc9998009cdb6fc29d
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-10-20 10:56:39 -0600

    Add url (and change pk to id throughout)

--- backend/authors/urls.py
+++ backend/authors/urls.py
@@ -5,6 +5,7 @@ from . import views
 # Set up endpoints for CRUD operations
 urlpatterns = [
     path("authors/", views.AuthorList.as_view()),
-    path("authors/<pk>/", views.AuthorDetail.as_view()),
-    path("authors/<pk>/followers/", views.FollowerList.as_view()),
+    path("authors/<id>/", views.AuthorDetail.as_view()),
+    path("authors/<id>/followers/", views.FollowerList.as_view()),
+    path("authors/<author_id>/followers/<follower_id>", views.FollowerDetail.as_view())
 ]
