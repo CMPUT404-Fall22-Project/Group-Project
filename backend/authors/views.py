207b2cb646ea42e8882289953944e9d0a8447b0a
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-10-19 23:01:03 -0600

    Handle GET call for followers of an author

--- backend/authors/views.py
+++ backend/authors/views.py
@@ -1,21 +1,16 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
 from django.http import Http404
-from rest_framework.response import Response
+from django.shortcuts import render
 from rest_framework import status
-from .models import Author
-from .serializers import AuthorSerializer, FollowerSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Author, Follower
+from .serializers import AuthorSerializer
 
 #  https://www.django-rest-framework.org/tutorial/3-class-based-views/
 
 # /authors/ GET, POST
 class AuthorList(APIView):
-
-    def get_object(self, pk):
-        try:
-            return Author.objects.get(pk=pk)
-        except Author.DoesNotExist:
-            raise Http404
         
     def get(self, request, fomat=None):
         authors = Author.objects.all()
@@ -41,6 +36,50 @@ class AuthorList(APIView):
 
 # /authors/<pk> GET, POST
 class AuthorDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Author.objects.get(pk=pk)
+        except Author.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        author = self.get_object(pk)
+        serializer = AuthorSerializer(author)
+        serializer_data = serializer.data
+        serializer_data["id"] = author.get_full_path()
+        serializer_data["url"] = author.get_full_path()
+        return Response(serializer_data, status=status.HTTP_200_OK)
+    
+
+class FollowerList(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Author.objects.get(pk=pk)
+        except Author.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk, fomat=None):
+        author = self.get_object(pk)
+        followers = author.followers.all()
+        serializer = AuthorSerializer(followers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    
+    # def post(self, request, format=None):
+
+    #     serializer = FollowerSerializer(data=request.data)
+    #      # if valid user input
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(status=status.HTTP_200_OK)
+    #     # else failed POST attempt
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# /authors/<pk> GET, POST
+class FollowerDetail(APIView):
 
     def get_object(self, pk):
         try:
