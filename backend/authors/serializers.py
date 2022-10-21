75ee95fcd13881cab5884061b8d05af71be820e7
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-10-20 18:29:52 -0600

    edited serializers after testing

--- backend/authors/serializers.py
+++ backend/authors/serializers.py
@@ -6,11 +6,14 @@ from .models import Author, Follower
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        # TODO: Bug when fields isn't set to "__all__":
-        # `HyperlinkedIdentityField` requires the request in the serializer context.
-        # Add `context={'request': request}` when instantiating the serializer.
-        # fields = ["type", "id", "host", "displayName", "url", "github", "profileImage"]
         fields = "__all__"
+
+class AuthorPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ["host", "displayName", "github", "profileImage"]
+
+
 
 class FollowerSerializer(serializers.ModelSerializer):
     class Meta:
