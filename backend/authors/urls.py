36fc0c6ea1952e0cc62dd54d7a9a25c989a0f32f
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-10-20 18:30:41 -0600

    added names for reverse lookup in tests

--- backend/authors/urls.py
+++ backend/authors/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from . import views
+from .views import AuthorDetail, AuthorList, FollowerDetail, FollowerList
 
 # Set up endpoints for CRUD operations
 urlpatterns = [
-    path("authors/", views.AuthorList.as_view()),
-    path("authors/<id>/", views.AuthorDetail.as_view()),
-    path("authors/<id>/followers/", views.FollowerList.as_view()),
-    path("authors/<author_id>/followers/<follower_id>", views.FollowerDetail.as_view())
+    path("authors/", AuthorList.as_view(), name="author_list"),
+    path("authors/<id>/", AuthorDetail.as_view, name="author_detail"),
+    path("authors/<id>/followers/", FollowerList.as_view()),
+    path("authors/<author_id>/followers/<follower_id>", FollowerDetail.as_view())
 ]
