bed4994e64c557cfd10b67050bb46bf8bfc221d4
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-10-21 15:45:51 -0600

    added to and edited author tests

--- backend/authors/tests.py
+++ backend/authors/tests.py
@@ -1,5 +1,5 @@
-from http.client import HTTPResponse
 from django.urls import include, path, reverse
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.test import APITestCase, URLPatternsTestCase
 from .models import Author
@@ -7,7 +7,6 @@ from .models import Author
 
 class AuthorTests(APITestCase, URLPatternsTestCase):
 
-    authors_url = "/authors/"
     test_author_data = {"host":"http://127.0.0.1:5454/",
                         "displayName":"Lara Croft",
                         "github":"http://github.com/laracroft",
@@ -17,45 +16,100 @@ class AuthorTests(APITestCase, URLPatternsTestCase):
         path('', include('authors.urls')),
     ]
 
+    def get_author_list_url(self):
+        return reverse("author_list")
+    
+    def get_author_detail_url(self,id):
+        return reverse("author_detail", args=[id])
+
+    
+    def get_full_path_for_test(self, id):
+        """ returns str(self.test_author_data["host"]) + "/authors/" + str(id)"""
+        return str(self.test_author_data["host"]) + "authors/" + str(id)
+    
 
     def test_get_from_empty_database(self):
         """Ensure {'type': 'authors', 'items': []} is returned when no authors are in db."""
-        response = self.client.get(self.authors_url, format='json')
+        response = self.client.get(self.get_author_list_url(), format='json')
         # ensure the proper response code is given
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 2)
         self.assertEqual(response.data["type"], "authors")
         self.assertEqual(type(response.data["items"]), list)
-        self.assertEqual(len(response.data["items"]), 0) 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data["items"]), 0)
 
         
     def test_post_new_author(self):
-        """Ensure an author object can be properly added to the db"""
-        url = self.authors_url
+        """Ensure an author object is properly added to the db"""
         # post the author
-        response = self.client.post(url, self.test_author_data, format='json')
+        response = self.client.post(self.get_author_list_url(), self.test_author_data, format='json')
         # ensure the proper response code is returned
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        
-        # get the posted author
-        response = self.client.get(self.authors_url, format='json')
+        # ensure the id for the posted author was returned
+        assert("id" in response.data.keys())
+        # ensure id is correct type
+        self.assertEqual(type(response.data["id"]), str)
+        # get all authors from db
+        response = self.client.get(self.get_author_list_url(), format='json')
         # ensure the proper response code is given
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # ensure there is now one author in the db
         self.assertEqual(len(response.data["items"]), 1)
-        serialized_author = response.data["items"][0]
-        data = dict(serialized_author)
 
-        # ensure all of our keys and values in our post request are contained in the returned author
+  
+    def test_get_posted_author_by_id(self):
+        # post the author
+        response = self.client.post(self.get_author_list_url(), self.test_author_data, format='json')
+        # ensure the proper response code is returned
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # ensure the id for the posted author was returned
+        assert("id" in response.data.keys())
+        id = response.data["id"]
+        url = self.get_author_detail_url(id)
+        # try to get the author with specified id
+        response = self.client.get(url, format='json')
+        # ensure the proper response code is given
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.data
+        # ensure the data properly corresponds with self.test_author_data
         for k in self.test_author_data.keys():
-            self.assertEqual(data[k], self.test_author_data[k])
+            self.assertEqual(self.test_author_data[k], response.data[k])
+        # ensure "id" is in response.data is in correctly edited format
+        correct_id = self.get_full_path_for_test(id)
+        self.assertEqual(correct_id, response.data["id"])
+        # ensure url is same as id
+        self.assertEqual(response.data["url"], response.data["id"])
 
-        # ensure that all of the proper default values are in the returned author object
-        self.assertEqual(data["followers"],[])
-        self.assertEqual(data["type"],"author")
-        self.assertEqual(data["isAuthorized"],False)
-        # ensure that returned id and url follow rubric specs
-        self.assertEqual(data["id"],data["url"])
-        # ensure that there are no extra keys
-        self.assertEqual(len(data.keys()), len(self.test_author_data.keys()) + 5) # 5 keys: "followers", "type", "isAuthorized", "id", "url"
+        # assert the object with id==id now exists
+        assert((type(get_object_or_404(Author, id=id)) == Author))
+        # get the existing object
+        author = get_object_or_404(Author, id=id)
+        # ensure the attributes match those of self.test_author_data
+        for k in self.test_author_data.keys():
+            self.assertEqual(self.test_author_data[k], getattr(author,k))
+
+
+
+
+
+
+
+    #     serialized_author = response.data["items"][0]
+    #     data = dict(serialized_author)
+
+    #     # ensure all of our keys and values in our post request are contained in the returned author data
+    #     for k in self.test_author_data.keys():
+    #         self.assertEqual(data[k], self.test_author_data[k])
+
+    #     # ensure that all of the proper default values are in the returned author object
+    #     self.assertEqual(data["followers"],[])
+    #     self.assertEqual(data["type"],"author")
+    #     self.assertEqual(data["isAuthorized"],False)
+    #     # ensure id and url are identical
+    #     self.assertEqual(data["id"], data["url"])
+    #     # ensure that the returned id and url follow the get_full_path() structure
+    #     correct_id = self.get_full_path_for_test(id)
+    #     self.assertEqual(correct_id, data["id"])
+    #     self.assertEqual(correct_id, data["url"])
+    #     # ensure that there are no extra keys
+    #     self.assertEqual(len(data.keys()), len(self.test_author_data.keys()) + 5) # 5 keys: "followers", "type", "isAuthorized", "id", "url"
