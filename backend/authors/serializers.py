75ffe4ea1054df4f3381ff625d63324a0667106e
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-10-21 21:38:35 -0600

    remove FollowerSerializer

--- backend/authors/serializers.py
+++ backend/authors/serializers.py
@@ -1,14 +1,8 @@
 from rest_framework import serializers
-from .models import Author, Follower
+from .models import Author
 
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = "__all__"
-
-
-class FollowerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Follower
         fields = "__all__"
