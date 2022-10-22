885100572dabec35004ed3555aeb4aac47a4d234
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-10-21 19:36:53 -0600

    add names to urls for reverse lookup

--- backend/authors/urls.py
+++ backend/authors/urls.py
@@ -6,6 +6,6 @@ from .views import AuthorDetail, AuthorList, FollowerDetail, FollowerList
 urlpatterns = [
     path("authors/", AuthorList.as_view(), name="author_list"),
     path("authors/<id>/", AuthorDetail.as_view(), name="author_detail"),
-    path("authors/<id>/followers/", FollowerList.as_view()),
-    path("authors/<author_id>/followers/<follower_id>", FollowerDetail.as_view())
+    path("authors/<id>/followers/", FollowerList.as_view(), name="follower_list"),
+    path("authors/<author_id>/followers/<follower_id>", FollowerDetail.as_view(), name="follower_detail")
 ]
