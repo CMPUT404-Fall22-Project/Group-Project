d450b78fd1be5763bd73555c7c2bf315b01fc126
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-10-22 11:59:28 -0600

    Created and installed posts-app

--- backend/project_404/settings.py
+++ backend/project_404/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_yasg',
-    'authors.apps.AuthorsConfig'
+    'authors.apps.AuthorsConfig',
+    'posts.apps.PostsConfig'
 ]
 
 MIDDLEWARE = [
