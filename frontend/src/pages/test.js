8ad2d831f3d28c5e22efd7dbf8930f2cf569c394
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-10-28 06:27:14 -0600

    added to amrit's FollowRequests

--- frontend/src/pages/test.js
+++ frontend/src/pages/test.js
@@ -1,152 +1,135 @@
-import React, { Component } from "react";
+import axios from "axios";
+import React, { Component, useEffect, useState } from "react";
 import { Button } from "@mui/material";
-import { withStyles } from "@mui/styles";
 import "./test.css";
-import { APPLICATION_NAME } from "../constants";
 import background from "../static/back.webp";
-import { useState } from "react";
-import cn from "classnames";
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import Box from '@mui/material/Box';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
+import Box from "@mui/material/Box";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
 import List from "@mui/material/List";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import DeleteIcon from "@mui/icons-material/Delete";
-import Stack from '@mui/material/Stack';
+import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material/styles";
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import { LikeButton } from "../components/likeButton";
+import Authentication from "../global/authentication";
+import Loader from "../components/loader";
 
+const Demo = styled("div")(({ theme }) => ({
+	backgroundColor: theme.palette.background.paper,
+}));
 
-export const LikeButton = () => {
-    const [liked, setLiked] = useState(null);
-    const [clicked, setClicked] = useState(false);
-  
-    return (
-      <button
-        onClick={() => {
-            setLiked(!liked);
-            setClicked(true);
-        }}
-        onAnimationEnd={() => setClicked(false)}
-        className={cn("like-button-wrapper", {
-          liked,
-          clicked,
-        })}
-      >
-        <div className="like-button">
-            <ThumbUpIcon />
-            <span>Like</span>
-            <span className={cn("suffix", { liked })}>d</span>
-        </div>
-      </button>
-    );
-  };
+export function TopAppBar() {
+	return (
+		<div style={{ width: "50%", height: "100%", position: "absolute" }}>
+			<Box sx={{ flexGrow: 1, maxWidth: 1000 }}>
+				<AppBar position="static">
+					<Toolbar>
+						{/* <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+							<MenuIcon />
+						</IconButton> */}
+						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+							Follow Requests
+						</Typography>
+					</Toolbar>
+				</AppBar>
+				<div style={{ width: "100%", height: "100%", position: "absolute", overflow: "auto" }}>
+					<FollowRequests></FollowRequests>
+				</div>
+			</Box>
+		</div>
+	);
+}
 
+export function FollowRequests() {
+	const userId = Authentication.getInstance().getUser().getId();
+	const [dense] = useState(false);
+	const [followRequests, setFollowRequests] = useState([]);
+	const [followerIds, setFollowerIds] = useState([]);
+	const [isLoaded, setIsLoaded] = useState(false);
 
-const LoginButton = withStyles((theme) => ({
-	root: {
-		color: "#575757",
-		backgroundColor: "#ffffff",
-		fontWeight: "100",
-		fontSize: "0.75rem",
-		width: "15em",
-		height: "3em",
-		"&:hover": {
-			backgroundColor: "#eeeeee",
-		},
-	},
-}))(Button);
+	useEffect(() => {
+		handleFollowRequests();
+	}, []);
 
-function generate(element: React.ReactElement) {
-    return [0, 1, 2, 3,4,5,6,7,8,9,10,11,12].map((value) =>
-      React.cloneElement(element, {
-        key: value
-      })
-    );
-  }
-  
-  const Demo = styled("div")(({ theme }) => ({
-    backgroundColor: theme.palette.background.paper
-  }));
+	const handleFollowRequests = async () => {
+		// Get all followers
+		response = await axios.get(process.env.REACT_APP_HOST + `authors/${userId}/followers/`);
+		console.log(response);
+		const followerIds = [];
+		for (let follower of response.data.items) {
+			followerIds.push(follower.id);
+		}
+		setFollowerIds(followerIds);
+		// Get all follow requests
+		var response = await axios.get(process.env.REACT_APP_HOST + `authors/${userId}/inbox/`);
+		console.log(response);
+		const followRequests = [];
+		for (let item of response.data.items) {
+			if (item.dataType === "Follow") {
+				const id = item.data.actor.id;
+				// Don't add follow requests from authors already following this author
+				if (!followerIds.includes(id)) {
+					followRequests.push(item.data);
+				}
+			}
+		}
+		setFollowRequests(followRequests);
+		setIsLoaded(true);
+	};
 
-  export function TopAppBar() {
-    return (
-      <div style={{ width: "50%", height: "100%", position: "absolute" }}>
-        <Box sx={{ flexGrow: 1,maxWidth: 1000 }}>
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 2 }}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                Follow Requests
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <div style={{ width: "100%", height: "100%", position: "absolute", overflow: "auto" }}>
-            <InteractiveList>
-            </InteractiveList>
-          </div>
-        </Box>
-      </div>
-    );
-  }
+	async function handleAcceptButton(followRequest) {
+		// PUT the Author with id === followerId as a follower of Author with id === userId
+		const followerId = followRequest.followerId;
+		const firstName = followRequest.summary.split(" ")[0];
+		axios
+			.put(process.env.REACT_APP_HOST + `authors/${userId}/followers/${followerId}`)
+			.then((res) => {
+				console.log(res);
+				alert(`${firstName} successfully added as a follower!`);
+			})
+			.catch((err) => console.log(err));
+	}
 
-  export function InteractiveList() {
-    const [dense] = React.useState(false);
-    const username = "username"
-    const name = "Firstname Lastname";
-  
-    return (
-      <Box sx={{ flexGrow: 1}}>
-        <Grid container spacing={0}>
-        </Grid>
-        <Grid container spacing={5}>
-          <Grid item xs={12} md={0}>
-            <Demo>
-              <List dense={dense}>
-                {generate(
-                  <ListItem
-                    secondaryAction={
-                      <Stack spacing={0.5} direction="row">
-                        <Button variant="outlined">Accept</Button>
-                        <Button variant="outlined">Reject</Button>
-                      </Stack>
-                    }
-                  >
-                    
-                    <ListItemAvatar>
-                      <Avatar>
-                        <DeleteIcon />
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText
-                      primary={username}
-                      secondary={name}
-                    />
-                  </ListItem>
-                )}
-              </List>
-            </Demo>
-          </Grid>
-        </Grid>
-      </Box>
-    );
-  }
-  
+	// prompt loader until followRequests are set
+	return !isLoaded ? (
+		<Loader />
+	) : (
+		<Box sx={{ flexGrow: 1 }}>
+			<Grid container spacing={0}></Grid>
+			<Grid container spacing={5}>
+				<Grid item xs={12} md={0}>
+					<Demo>
+						<List dense={dense}>
+							{followRequests.map((followRequest) => (
+								<ListItem
+									secondaryAction={
+										<Stack spacing={0.5} direction="row">
+											<Button variant="outlined" onClick={() => handleAcceptButton(followRequest)}>
+												Accept
+											</Button>
+											<Button variant="outlined">Reject</Button>
+										</Stack>
+									}
+								>
+									<ListItemAvatar>
+										<Avatar alt="actor" src={followRequest.actor.profileImage}></Avatar>
+									</ListItemAvatar>
+									<ListItemText primary={followRequest.summary} key={followRequest.actor.id} />
+								</ListItem>
+							))}
+						</List>
+					</Demo>
+				</Grid>
+			</Grid>
+		</Box>
+	);
+}
 
 class LoginComponent extends Component {
 	constructor(props) {
@@ -159,7 +142,6 @@ class LoginComponent extends Component {
 	login() {}
 
 	render() {
-
 		return (
 			<div
 				className="Fade-In"
@@ -175,20 +157,16 @@ class LoginComponent extends Component {
 				}}
 			>
 				<div style={{ height: "50%", minHeight: "1250px", position: "relative" }}>
-        <LikeButton className="like-button-wrapper">
-          Like
-          </LikeButton>
+					<LikeButton className="like-button-wrapper">Like</LikeButton>
 					<div className="outer-login-box">
-              <div classname="follower-requests">
-                <TopAppBar classname="top-app-bar">
-                </TopAppBar>
-              </div>
-            </div>
+						<div classname="follower-requests">
+							<TopAppBar classname="top-app-bar"></TopAppBar>
+						</div>
 					</div>
 				</div>
+			</div>
 		);
 	}
 }
 
 export default LoginComponent;
-
