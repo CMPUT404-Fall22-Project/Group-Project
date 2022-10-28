85575376e82196c145bae973fc41dd8623eb3026
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-10-27 18:34:02 -0600

    add temp path "/temp-follow-request"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,10 @@ import ApplicationError from "./pages/applicationError";
 import HTML404 from "./components/errors/404";
 import React, { Component } from "react";
 import LoginComponent from "./pages/login";
+import TestPageComponent from "./pages/testPage";
 import "./App.css";
 import history from "./history";
+import FollowRequest from "./components/followRequest";
 
 class App extends Component {
 	static _ERROR_DATA = [];
@@ -32,6 +34,7 @@ class App extends Component {
 							<Route render={(props) => <ApplicationError {...props} error={App._ERROR_DATA} />} />
 						) : null}
 						<Route exact path="/" render={(props) => <LoginComponent {...props} />} />
+						<Route exact path="/temp-follow-request" render={(props) => <FollowRequest {...props} />} />
 						<Route component={HTML404}></Route>
 					</Switch>
 				</Router>
