2ebdacb4858f8f3b0b7d1ad0d02b5e746ce1e7cc
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-11-03 12:04:16 -0600

    Add InboxDetail url

--- backend/inbox/urls.py
+++ backend/inbox/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import InboxList
+from .views import InboxList, InboxDetail
 
 urlpatterns = [
     path("authors/<id>/inbox/", InboxList.as_view(), name="inbox_list"),
+    path("authors/<author_id>/inbox/<inbox_id>", InboxDetail.as_view(), name="inbox_detail")
 ]
