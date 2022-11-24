c27d9e01deca34dd686da909d9884b819167c48b
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-11-24 02:02:15 -0700

    Edit Node Modal and makemigration

--- backend/nodes/models.py
+++ backend/nodes/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
-# Create your models here.
-
 class Node(models.Model):
-    host = models.URLField(blank=False, editable=False)
+    host = models.URLField(blank=False)
+    username = models.CharField(max_length=255)  
+    password = models.CharField(max_length=255)  
+    
+    def __str__(self):
+        return self.host
