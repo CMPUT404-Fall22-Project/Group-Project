477aaf9779b3927bfa6ca738a9fd14b46891b1ac
MdderUofA <mdder@ualberta.ca>
2022-11-26 21:38:22 -0700

    Fixes for authentication

--- backend/project_404/middleware/auth_middleware.py
+++ backend/project_404/middleware/auth_middleware.py
@@ -1,5 +1,5 @@
 
-from authentication.models import Session
+from authentication.models import Session, ExternalNode
 from django.http import HttpResponseBadRequest
 
 skip_paths = ["/swagger", "redoc/"]
@@ -17,28 +17,35 @@ class AuthMiddleware:
             return self.get_response(request)
 
         request.app_session = None
-        auth = None
-        token = None
+
+        authorized = False
+        auth_error = HttpResponseBadRequest("Unauthorized", status=401)
         if "Authorization" in request.headers:
             auth = request.headers["Authorization"]
+            split_auth = auth.split()
+            if len(split_auth) < 2:
+                auth_error = HttpResponseBadRequest("Malformed Authroization header. Expecting BASIC xxx or TOKEN xxx", status=403)
+            elif split_auth[0].lower() == "token":
+                token = split_auth[1]  # extract token
+                try:
+                    sess = Session.objects.get(pk=token)
+                    request.app_session = sess
+                    authorized = True
+                except:
+                    auth_error = HttpResponseBadRequest("Bad token", status=403)
+            elif split_auth[0].lower() == "basic":
+                user_pass = split_auth[1]
+                try:
+                    ExternalNode.object.get(authorization=user_pass)
+                    authorized = True
+                except:
+                    auth_error = HttpResponseBadRequest("Bad username or password", status=403)
 
-        if auth and auth.startswith("Token "):
-            token = auth.split()[1]  # extract token
-
-        if not token:
+        if not authorized:
             allowed = any([request.path_info.startswith(x) for x in allowed_paths])
-            allowed = True #TODO: delete this!
             if not allowed:
-                resp = HttpResponseBadRequest("Missing token", status=401)
-                resp["WWW-Authenticate"] = "Token"
-                return resp
-
-        if token:
-            try:
-                sess = Session.objects.get(pk=token)
-                request.app_session = sess
-            except:
-                resp = HttpResponseBadRequest("Bad token", status=403)
+                resp = auth_error
+                resp["WWW-Authenticate"] = "Basic, Token"
                 return resp
 
         return self.get_response(request)
