808d19a83ef1d0950de7726fb890d0c6cfa025e6
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-11-26 17:24:56 -0700

    Add author, rather than authorID to history.push()

--- frontend/src/components/authorSearch.js
+++ frontend/src/components/authorSearch.js
@@ -15,6 +15,7 @@ import history from "../history";
 export default function AuthorSearch() {
 	const [authorId, setAuthorId] = useState("");
 	const [authors, setAuthors] = useState([]);
+	const [autoCompleteAuthors, setAutoCompleteAuthors] = useState([]);
 	const userId = Authentication.getInstance().getUser().getId();
 
 	const [anchorEl, setAnchorEl] = useState();
@@ -37,7 +38,8 @@ export default function AuthorSearch() {
 		}
 		// Remove the logged in author from arr
 		arr = arr.filter((a) => a.id !== userId);
-		setAuthors(arr);
+		setAutoCompleteAuthors(arr);
+		setAuthors(authors);
 	};
 
 	const handleButtonClick = (event) => {
@@ -45,7 +47,8 @@ export default function AuthorSearch() {
 	};
 
 	const viewProfile = () => {
-		history.push({ pathname: "/authors/" + authorId.split("/authors/")[1], state: { authorId: authorId } });
+		const author = authors.find((e) => e.id == authorId);
+		history.push({ pathname: "/authors/" + authorId.split("/authors/")[1], state: { author: author } });
 		handleClose();
 	};
 
@@ -59,7 +62,7 @@ export default function AuthorSearch() {
 					freeSolo
 					id="free-solo-2-demo"
 					disableClearable
-					options={authors}
+					options={autoCompleteAuthors}
 					style={{
 						margin: "0.25em",
 						width: "25em",
