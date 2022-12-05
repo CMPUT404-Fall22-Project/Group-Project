d193c789b36a055ec4298c7ecf86abfa7ccebfb2
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-12-05 11:56:01 -0700

    Handle like logic

--- frontend/src/components/likeButton.js
+++ frontend/src/components/likeButton.js
@@ -17,11 +17,11 @@ const theme = createTheme({
 
 export const LikeButton = (props) => {
 	var like = "Like";
+	var liked = "Liked";
 	// var unlike = "Unlike";
 	const [buttonText, setButtonText] = useState("");
 	const userId = props.userId;
 	const author = props.author;
-	const authorId = props.author.getId();
 	const postId = props.postId;
 
 	useEffect(() => {
@@ -29,24 +29,22 @@ export const LikeButton = (props) => {
 	}, []);
 
 	async function handleButtonText() {
-		// Check if author already has a pending follow request from user
-		var response = await axios.get(`${authorId}/inbox/`);
-		for (let item of response.data.items) {
-			// Rubric states "Follow"
-			if (["like"].includes(item.dataType)) {
-				// if follow request sender is the user
-				if (userId === item.data.id && item.data.post === postId) {
-					setButtonText("Liked");
-					return;
-				}
+		// get all objects (posts and comments) the user has liked
+		var response = await axios.get(`${userId}` + "/liked/");
+		if (response.status == 200) {
+			var likedArray = response.data.items;
+			// if this post is in the user's list of liked objects
+			if (likedArray.find((e) => e.object === postId)) {
+				setButtonText(liked);
+				return;
 			}
+			setButtonText(like);
 		}
-		setButtonText("Like");
 	}
 
 	async function handleButtonClick() {
 		// Handles like and unlike
-		if (buttonText === "Like") {
+		if (buttonText === like) {
 			var handleLikeResponse = await axios.post(process.env.REACT_APP_HOST + "handle-like/", {
 				senderAuthorURL: userId,
 				receiverAuthor: { url: author.getId(), displayName: author.getUsername() },
@@ -54,13 +52,14 @@ export const LikeButton = (props) => {
 			});
 			if (handleLikeResponse.status <= 201) {
 				NotificationBar.getInstance().addNotification("Liked successfully!", NotificationBar.NT_SUCCESS);
-				setButtonText("Liked");
+				setButtonText(liked);
 			} else {
 				NotificationBar.getInstance().addNotification(handleLikeResponse.err, NotificationBar.NT_ERROR);
 			}
 			return;
 		}
 
+		// UNLIKE DISABLED
 		// remove userId as a liker of post
 		// var response = await axios.delete(`${authorId}/inbox/`, { id: userId, type: "like" });
 		// if (response.status === 200) {
@@ -75,12 +74,7 @@ export const LikeButton = (props) => {
 	) : (
 		<div>
 			<ThemeProvider theme={theme}>
-				<Button
-					size="medium"
-					variant="contained"
-					onClick={handleButtonClick}
-					// disabled={buttonText === "Request Sent..."}
-				>
+				<Button size="medium" variant="contained" onClick={handleButtonClick} disabled={buttonText === liked}>
 					{buttonText}
 				</Button>
 			</ThemeProvider>
