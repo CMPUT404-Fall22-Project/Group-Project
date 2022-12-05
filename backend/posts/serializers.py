32631aa6f836f9b3d79fb6e19211b8b4a595505b
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-12-05 16:16:36 -0700

    undo serializer idea

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Post, Comment, Like
+from authors.models import Author
 from authors.serializers import AuthorSerializer
+from django.shortcuts import get_object_or_404
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -15,16 +17,16 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class LikeSerializer(serializers.ModelSerializer):
-    author = serializers.SerializerMethodField()
-    summary = serializers.SerializerMethodField()
+    # author = serializers.SerializerMethodField()
+    # summary = serializers.SerializerMethodField()
 
     class Meta:
         model = Like
-        fields = ["context","type","object","author","summary"]
+        fields = ["context","type","object","author"]
 
-    def get_author(self, obj: Like):
-        author = AuthorSerializer(obj.author).data       
-        return author
+    # def get_author(self, obj: Like):
+    #     author = AuthorSerializer(obj.author).data       
+    #     return author
 
-    def get_summary(self, obj: Like):
-        return f"{obj.author.displayName} Likes your post"
+    # def get_summary(self, obj: Like):
+    #     return f"{obj.author.displayName} Likes your post"
