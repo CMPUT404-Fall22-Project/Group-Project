cf42f74a6553a1eee287f85d4d690ffb44a7a5ba
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-12-05 06:24:31 -0700

    Edit serializer

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from .models import Post, Comment, CommentLike, PostLike
+from authors.models import Author
+from authors.serializers import AuthorSerializer
+from django.shortcuts import get_object_or_404
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -10,12 +13,24 @@ class PostSerializer(serializers.ModelSerializer):
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = "__all__"
+        fields = ["context","type","object","author"]
+
 
 class PostLikeSerializer(serializers.ModelSerializer):
+    author = serializers.SerializerMethodField()
+    summary = serializers.SerializerMethodField()
+
     class Meta:
         model = PostLike
-        fields = "__all__"
+        fields = ["context","type","object","author","summary"]
+
+    def get_author(self, obj: PostLike):
+        author = AuthorSerializer(obj.author).data       
+        return author
+
+    def get_summary(self, obj: PostLike):
+        return f"{obj.author.displayName} Likes your post"
+
 
 class CommentLikeSerializer(serializers.ModelSerializer):
     class Meta:
